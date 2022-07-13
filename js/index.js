3e36749d1374268f701df8552e5569e96b86ef96
vusal <vusalr06@gmail.com>
2022-07-13 19:28:41 +0400

    changes on style

--- js/index.js
+++ js/index.js
@@ -5,8 +5,8 @@ $('.btn-load-comments').click(function () {
         event.parent().parent().children('.read-comment-section').remove();
         event.val('close');
     } else {
-        let html = "        <div class=\"read-comment-section m-2 p-2\">\n" +
-            "            <div class=\"comment-container m-4\">\n" +
+        let html = "        <div class=\"read-comment-section m-1 p-2\">\n" +
+            "            <div class=\"comment-container m-3\">\n" +
             "                <span class=\"comment-user\"><span>@</span>vrehimov:</span>&nbsp;\n" +
             "                <p class=\"comment-text\">Some comments in here Some comments in here Some Some comments in here Some\n" +
             "                    comments in here Some comments iSome comments in here Some comments in here Some comments iSome\n" +
@@ -14,7 +14,7 @@ $('.btn-load-comments').click(function () {
             "                    comments in here Some comments in here </p>\n" +
             "            </div>\n" +
             "        </div>";
-        event.parent().parent().append(html);
+        event.parent().parent().append(html + html);
         event.parent().parent().children('.write-comment-section').show();
         event.val('open');
     }
